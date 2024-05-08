81809bc97a75ea2f9eed9103ea1aa28d9783bc29
ayushthe1 <ayushsharmaa101@gmail.com>
2024-05-08 20:59:54 +0530

    define models

---
+++ models/blog.go
@@ -0,0 +1,12 @@
+package models
+
+type Blog struct {
+	Id     uint   `json:"id"`
+	Title  string `json:"title" validate:"required"`
+	Desc   string `json:"desc" validate:"required"`
+	Image  string `json:image`
+	UserID string `json:"userid" validate:"required"`
+	User   User   `json:"user";gorm:"foreignkey:UserID"`
+}
+
+//The gorm:"foreignkey:UserID" tag indicates that the UserID field is a foreign key that references the primary key of the User entity. This allows GORM to automatically establish a relationship between the Blog and User tables in the database.
