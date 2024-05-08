ebe076222aea77ca8b6cf468f0fcd3a2b56764a9
ayushthe1 <ayushsharmaa101@gmail.com>
2024-05-08 21:09:33 +0530

    define user controller

--- controller/userController.go
+++ controller/userController.go
@@ -1,1 +1,115 @@
 package controller
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/ayushthe1/blog-backend/database"
+	"github.com/ayushthe1/blog-backend/models"
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+)
+
+var validate = validator.New()
+
+func Register(c *fiber.Ctx) error {
+
+	var data models.User
+	var userData models.User
+	if err := c.BodyParser(&data); err != nil {
+		fmt.Println("Unable to parse bodyy")
+		fmt.Println(err.Error())
+		return err
+	}
+
+	validationErr := validate.Struct(data)
+	if validationErr != nil {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{"error": validationErr.Error()})
+
+	}
+
+	//Check if email already exist in database
+	database.DB.Where("email=?", strings.TrimSpace(data.Email)).First(&userData)
+	if userData.Id != 0 {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "Email already exist",
+		})
+
+	}
+
+	user := models.User{
+		FirstName: data.FirstName,
+		LastName:  data.LastName,
+		Phone:     data.Phone,
+		Email:     strings.TrimSpace(data.Email),
+	}
+	user.SetPassword(data.Password)
+	err := database.DB.Create(&user)
+	if err != nil {
+		log.Println(err)
+	}
+	c.Status(200)
+	return c.JSON(fiber.Map{
+		"user":    user, // remove this later
+		"message": "Account created successfullys",
+	})
+
+}
+
+func Login(c *fiber.Ctx) error {
+	var data models.User
+
+	if err := c.BodyParser(&data); err != nil {
+		fmt.Println("Unable to parse body")
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{"error": err.Error()})
+	}
+
+	validationErr := validate.Struct(data)
+	if validationErr != nil {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{"error": validationErr.Error()})
+
+	}
+
+	var user models.User
+	database.DB.Where("email=?", data.Email).First(&user)
+	if user.Id == 0 {
+		c.Status(404)
+		return c.JSON(fiber.Map{
+			"message": "Email Address doesn't exit, kindly create an account",
+		})
+	}
+	if err := user.ComparePassword(data.Password); err != nil {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "incorrect password",
+		})
+	}
+
+	token, err := util.GenerateJwt(strconv.Itoa(int(user.Id)))
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError)
+		return nil
+	}
+
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(time.Hour * 24),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+
+	return c.JSON(fiber.Map{
+		"message": "you have successfully login",
+		"user":    user,
+	})
+
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ayushthe1/blog-backend
 go 1.18
 
 require (
+	github.com/go-playground/validator/v10 v10.20.0
 	github.com/gofiber/fiber/v2 v2.52.4
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.23.0
@@ -12,11 +13,15 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
@@ -24,5 +29,7 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
 )
